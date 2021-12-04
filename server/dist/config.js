2bc535f67eb9f3f39f25003e14a758c6cb714b33
Drincann <1019933576@qq.com>
2021-12-05 01:01:51 +0800

    chore(gitignore): 忽略 dist

--- server/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    serverPort: process.env.GOTO_PORT || 8080,
-    mongodbUrl: process.env.GOTO_MONGOURL || 'mongodb://MongoDB:27017/goto',
-};
