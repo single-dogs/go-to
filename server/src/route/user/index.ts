0d5d4d8f92915782b30bfb11217f5b80154964f4
Drincann <1019933576@qq.com>
2021-12-06 14:25:12 +0800

    feature(GET /user /users): 实现了查询用户的路由

--- server/src/route/user/index.ts
+++ server/src/route/user/index.ts
@@ -2,9 +2,12 @@ import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import { register } from './register'
 import { update } from './update'
+import { query, queryMultiUser } from './query'
 import { jwtAuth } from '../../middleware/jwtAuth'
 import { Context, DefaultState } from 'koa'
 
 export const userRouter = new Router<DefaultState, Context>()
     .post('/user', bodyParser(), register)
     .put('/user', jwtAuth, bodyParser(), update)
+    .get('/user', query)
+    .get('/users', queryMultiUser)
---
+++ server/src/route/user/query.ts
@@ -0,0 +1,119 @@
+import Koa from 'koa'
+import joi from 'joi'
+import { MongoUser } from '../../model'
+import { pick } from 'lodash'
+
+const queryValidator = joi.object({
+    _id: joi.string(),
+    username: joi.string(),
+})
+export async function query(ctx: Koa.Context) {
+    try {
+
+        const { _id, username } = ctx.query
+        const { error } = queryValidator.validate({ _id, username })
+        if (error || (_id == undefined && username == undefined)) {
+            ctx.body = {
+                code: 1,
+                message: '参数错误',
+                data: null
+            }
+            return
+        }
+
+        let mongoUser = null
+        if (username) {
+            mongoUser = await MongoUser.fromUsername(username as string)
+        } else {
+            mongoUser = await MongoUser.fromId(_id as string)
+        }
+
+        if (mongoUser == null) {
+            ctx.body = {
+                code: 1,
+                message: '用户不存在',
+                data: null
+            }
+            return
+        }
+
+        ctx.body = {
+            code: 0,
+            message: '查询成功',
+            data: pick(mongoUser, ['_id', 'username'])
+        }
+    } catch (error: any) {
+        ctx.body = {
+            code: 1,
+            message: error?.message,
+            data: null
+        }
+
+    }
+
+}
+
+const queryMultiUserValidator = joi.object({
+    _ids: joi.array().items(joi.string()),
+    usernames: joi.array().items(joi.string()),
+})
+
+export async function queryMultiUser(ctx: Koa.Context) {
+    try {
+        const { _ids, usernames } = ctx.query
+        const { error } = queryMultiUserValidator.validate({ _ids, usernames })
+        _ids as string[] | null
+        usernames as string[] | null
+        if (error || (_ids == undefined && usernames == undefined)) {
+            ctx.body = {
+                code: 1,
+                message: '参数错误',
+                data: null
+            }
+            return
+        }
+
+        const responseData: {
+            _idMap: {
+                [_id: string]: {
+                    _id?: string, username?: string
+                } | null
+            } | null,
+            usernameMap: {
+                [username: string]: {
+                    _id: string, username: string
+                } | null
+            } | null
+        } = { _idMap: null, usernameMap: null }
+
+        if (_ids) {
+            responseData._idMap = {}
+            for (const _id of _ids) {
+                const mongoUser = await MongoUser.fromId(_id)
+                responseData._idMap[_id] = mongoUser ? { _id: mongoUser._id.toString(), username: mongoUser.username } : null
+            }
+        }
+
+        if (usernames) {
+            responseData.usernameMap = {}
+            for (const username of usernames) {
+                const mongoUser = await MongoUser.fromUsername(username)
+                responseData.usernameMap[username] = mongoUser ? { _id: mongoUser._id.toString(), username: mongoUser.username } : null
+            }
+        }
+
+        ctx.body = {
+            code: 0,
+            message: '查询成功',
+            data: responseData
+        }
+    } catch (error: any) {
+        ctx.body = {
+            code: 1,
+            message: error?.message,
+            data: null
+        }
+
+    }
+
+}
