07e0643395afdd309368ac64b2a41a87bb375234
Drincann <1019933576@qq.com>
2021-12-29 22:12:01 +0800

    feature(back): 在修改用户接口 PUT /user 前添加密码验证中间件

--- server/src/route/user/index.ts
+++ server/src/route/user/index.ts
@@ -5,9 +5,10 @@ import { update } from './update'
 import { query, queryMultiUser } from './query'
 import { UserLoginedAuthMiddleware } from '../../middleware/jwtAuth'
 import { Context, DefaultState } from 'koa'
+import { loginAuth } from '../../middleware/loginAuth'
 
 export const userRouter = new Router<DefaultState, Context>()
     .post('/user', bodyParser(), register)
-    .put('/user', UserLoginedAuthMiddleware, bodyParser(), update)
+    .put('/user', UserLoginedAuthMiddleware, loginAuth, bodyParser(), update)
     .get('/user', query)
     .get('/users', queryMultiUser)
