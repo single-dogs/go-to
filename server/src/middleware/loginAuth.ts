d6c1130e1cd1411c17199cb9b10ed896e29e5920
Drincann <1019933576@qq.com>
2021-12-29 22:11:15 +0800

    fix(back):修复 密码验证中间件会通过任意密码的问题

--- server/src/middleware/loginAuth.ts
+++ server/src/middleware/loginAuth.ts
@@ -2,8 +2,7 @@ import Koa from "koa";
 import { MongoUser } from "../model/User"
 
 export async function loginAuth(ctx: Koa.Context, next: Koa.Next) {
-    if (!(await MongoUser.fromId(ctx.jwtdata.userinfo?._id))
-        ?.comparePassword(ctx.jwtdata.userinfo?.password)) {
+    if (! await (await MongoUser.fromId(ctx.state.userinfo?._id))?.comparePassword(ctx.state.userinfo?.password)) {
         ctx.body = {
             code: 1,
             message: "登录信息失效",
@@ -11,4 +10,5 @@ export async function loginAuth(ctx: Koa.Context, next: Koa.Next) {
         }
         return
     }
+    await next()
 }
